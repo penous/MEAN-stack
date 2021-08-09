bb760129ddbd3b20b819a8b1f9d01128b7915118
Tom Dirinck <tom.dirinck@gmail.com>
2021-08-09 15:41:44 +0200

    Added friend model.

--- client/src/app/models/friend.ts
+++ client/src/app/models/friend.ts
@@ -1,2 +1,9 @@
 export class Friend {
+  constructor(
+    public firstname: string,
+    public lastname: string,
+    public email: string,
+    public phone: string,
+    public language: string
+  ) {}
 }
